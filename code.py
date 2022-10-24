2596447beb770d2d77b953570082282ab38baa54
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2022-10-24 11:18:58 -0400

    Create code.py

---
+++ code.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created by: Venika Sem
+# Created on: Oct 2022
+# This program is the "Space Aliens" program  on the PyBadge
+
+import ugame
+import stage
+
+def game_scene():
+    # This function is the main game game_scene
+
+    print("\n\n\n") # 3 blank lines
+    print("Hello, World!")
+
+    print("\nDone.")
+
+    # repeat forever, game loop
+    while True:
+        pass # just a placeholder for now
+
+if __name__ == "__main__":
+    game_scene()
