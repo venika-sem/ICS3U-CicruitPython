60c324cebd51d146e3876bd5d4217fe11479b37c
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2022-11-01 11:08:15 -0400

    changed

--- code.py
+++ code.py
@@ -13,23 +13,35 @@ def game_scene():
 
     # image banks for Circuit Python
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
     # set the background to image 0 in the image bank
     # and the size (10x8 tiles of size 16x16)
     background = stage.Grid(image_bank_background, 10, 8)
 
+    # a sprite that will be updated every frame
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+
     # create a stage for the background to show up on
     # and set the frame rate to 60 fps
     game = stage.Stage(ugame.display, 60)
+
     # set the layers of all sprites, items show up in order
-    game.layers = [background]
+    game.layers = [ship] + [background]
+
     # render all sprites
     # most likely you will only render the background once per game scene
     game.render_block()
 
     # repeat forever, game loop
     while True:
-        pass  # just a placeholder for now
+        # get user input
+
+        # update game logic
+
+        # redraw Sprite
+        game.render_sprites([ship])
+        game.tick()  # wait until refresh rate finishes
 
 
 if __name__ == "__main__":
