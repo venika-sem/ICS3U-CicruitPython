ce5bf4b5f9a2e4ff3e288d46c0f7cfdb92ad628a
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2022-11-02 11:05:41 -0400

    changed

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Created by: Venika Sem
-# Created on: Oct 2022
+# Created on: Nov 2022
 # This program is the "Space Aliens" program  on the PyBadge
 
 import stage
@@ -36,7 +36,24 @@ def game_scene():
     # repeat forever, game loop
     while True:
         # get user input
+        keys = ugame.buttons.get_pressed()
 
+        if keys & ugame.K_X:
+            print("A")
+        if keys & ugame.K_O:
+            print("B")
+        if keys & ugame.K_START:
+            print("Start")
+        if keys & ugame.K_SELECT:
+            print("Select")
+        if keys & ugame.K_RIGHT:
+            ship.move(ship.x + 1, ship.y)
+        if keys & ugame.K_LEFT:
+            ship.move(ship.x - 1, ship.y)
+        if keys & ugame.K_UP:
+            ship.move(ship.x, ship.y - 1)
+        if keys & ugame.K_DOWN:
+            ship.move(ship.x, ship.y + 1)
         # update game logic
 
         # redraw Sprite
